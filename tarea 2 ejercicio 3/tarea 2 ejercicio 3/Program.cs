1b8e78bded14a7103f9ba47578d8acedb8504ce7
KaUnf18 <145079280+KaUnf18@users.noreply.github.com>
2023-10-01 00:34:44 -0600

    Add files via upload

---
+++ tarea 2 ejercicio 3/tarea 2 ejercicio 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_2_ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declaración de variables
+            int cantidadDeArticulos;
+            double precioPorArticulo;
+            double total;
+
+            // Entrada de datos
+            Console.Write("Ingrese la cantidad de artículos: ");
+            if (!int.TryParse(Console.ReadLine(), out cantidadDeArticulos) || cantidadDeArticulos < 0)
+            {
+                Console.WriteLine("Por favor, ingrese una cantidad válida.");
+                return;
+            }
+
+            // Lógica para determinar el precio por artículo
+            if (cantidadDeArticulos <= 10)
+            {
+                precioPorArticulo = 20.0;
+            }
+            else
+            {
+                precioPorArticulo = 15.0;
+            }
+
+            // Cálculo del total
+            total = cantidadDeArticulos * precioPorArticulo;
+
+            // Salida de resultados
+            Console.WriteLine($"Precio por artículo: ${precioPorArticulo}");
+            Console.WriteLine($"Total a pagar: ${total}");
+            Console.ReadLine();
+        }
+    }
+}
+  
